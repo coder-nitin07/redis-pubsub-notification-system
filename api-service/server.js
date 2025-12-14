a4155056f9f16a52d39de4f8a27000973b5d5a10
coder-nitin07 <nitinthakur87504@gmail.com>
2025-12-14 15:20:49 +0530

    Create publisher and add routes

--- api-service/server.js
+++ api-service/server.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const redis = require('./publisher');
 const app = express();
 require('dotenv').config();
 
 app.use(express.json());
 
+// event
+app.post('/event', async (req, res)=>{
+    try {
+        const { userId, email } = req.body;
+        
+        const eventData = {
+            userId, 
+            email,
+            timeStamp: Date.now()
+        }
+
+        await redis.publish(
+            'user.created',
+            JSON.stringify(eventData)
+        )
+
+        res.json({ message: 'Event Published' });
+    } catch (err) {
+        console.log('Something went wrong : ', err);
+    }
+});
+
+// test route
 app.get('/health', (req, res)=>{
     res.json({ status: 'API Service running' });
 });
